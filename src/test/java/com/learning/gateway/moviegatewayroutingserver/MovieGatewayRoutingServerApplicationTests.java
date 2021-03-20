a873eb0c1166d9a5ca6017be7fe1eedc6d1f8826
Akhil Anilkumar <akhil.slipknot@hotmail.com>
2021-03-20 22:10:48 +0530

    Initial commits

---
+++ src/test/java/com/learning/gateway/moviegatewayroutingserver/MovieGatewayRoutingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.gateway.moviegatewayroutingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGatewayRoutingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
