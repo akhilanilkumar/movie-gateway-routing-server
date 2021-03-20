8e9b6bcebc8c37c8bd53131e4a1a99c327e1cf2b
Akhil Anilkumar <akhil.slipknot@hotmail.com>
2021-03-21 01:10:10 +0530

    Spring security added. cloud gateway added, need to fix the service discovery with Eureka server.

--- src/main/java/com/learning/gateway/moviegatewayroutingserver/MovieGatewayRoutingServerApplication.java
+++ src/main/java/com/learning/gateway/moviegatewayroutingserver/MovieGatewayRoutingServerApplication.java
@@ -2,8 +2,10 @@ package com.learning.gateway.moviegatewayroutingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieGatewayRoutingServerApplication {
 
 	public static void main(String[] args) {
